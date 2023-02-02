5ff16f0111c40786759cdefe04e2838b79d32e50
João Pedro R. Diniz <jp_rd@hotmail.com>
2023-02-02 18:47:41 -0300

    Removida annotation @With dá classe User.

--- src/main/java/com/example/domains/User.java
+++ src/main/java/com/example/domains/User.java
@@ -1,6 +1,9 @@
 package com.example.domains;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -15,7 +18,6 @@ import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Data
-@With
 @Entity
 @Builder
 @NoArgsConstructor
